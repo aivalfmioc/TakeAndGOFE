0685a2dc226745bbec0761485cddba408eae4d16
poppaula <poppaula0106@gmail.com>
2023-01-08 01:53:12 +0200

    basic map with 2 hardcoded markers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,14 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit{
   zoom = 15;
+  markers :any[] = [];
   center!: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
-    mapTypeId: 'hybrid',
+    mapTypeId: 'roadmap',
     zoomControl: false,
-    scrollwheel: false,
+    scrollwheel: true,
     disableDoubleClickZoom: true,
+    clickableIcons: false,
     maxZoom: 20,
     minZoom: 8,
+    mapTypeControl: false,
   };
   ngOnInit() {
     
@@ -22,5 +25,41 @@ export class MapComponent implements OnInit{
         lat: 45.7494,
         lng: 21.2272,
       };
+      this.markers.push({
+        id:1,
+        position: {
+          lat: 45.7494,
+          lng: 21.2272,
+        },
+        label: {
+          color: 'black',
+          text: 'Marker label ' + (this.markers.length + 1),
+          fontWeight: 'bold',
+        },
+        title: 'Marker title ' + (this.markers.length + 1),
+        options:{
+          clickable: true,
+          animation: google.maps.Animation.BOUNCE
+        }
+      });
+      this.markers.push({
+        id:2,
+        position: {
+          lat: 45.7450,
+          lng: 21.2272,
+        },
+        label: {
+          color: 'black',
+          text: 'Marker label ' + (this.markers.length + 1),
+          fontWeight: 'bold',
+        },
+        title: 'Marker title ' + (this.markers.length + 1),
+        clickable: true,
+        animation: google.maps.Animation.BOUNCE
+      });
+  }
+
+  markerClicked(id: number){
+    console.log(id);
   }
 }
