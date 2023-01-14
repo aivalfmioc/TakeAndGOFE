48ed497880c9abf6d341772673de5f9e6aba490d
flavia <flaviamioc@yahoo.com>
2023-01-15 00:20:00 +0200

    styled authentication page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injectable, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit,  ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
@@ -7,22 +7,12 @@ import { UserService } from '../services/user.service';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-
-    template: `
-      <style>
-        button {
-          background-color: white;
-          border: 1px solid #777;
-        }
-      </style>
-      <h3>Controls</h3>
-      <button type="button" (click)="activate()">Activate</button>
-    `
-  
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit{
   user : User = new User();
-  error_message : String = '';
+  error_message : String = "";
+  valid: Boolean = false;
   constructor(public router: Router, public userService: UserService){}
   set(){
     localStorage.setItem('user','hello');
@@ -34,6 +24,30 @@ export class LoginComponent implements OnInit{
     localStorage.removeItem('user');
     window.location.reload();
   }
+
+  passwordhasError(){
+    const value = this.user.password;
+    if(!value)
+      return false;
+    const hasUpperCase = /[A-Z]+/.test(value);
+    const hasLowerCase = /[a-z]+/.test(value);
+    const hasNumeric = /[0-9]+/.test(value);
+    const has8Characters = value.length >= 8;
+
+    const passwordValid = hasUpperCase && hasLowerCase && hasNumeric && has8Characters;
+    this.valid = passwordValid;
+    return !(passwordValid && value);
+  }
+  passwordsNotMatchingError(){
+    const value1 = this.user.checkPassword;
+    const value2 = this.user.password;
+    this.valid = (value1==value2);
+    return value1!==value2 && value1;
+  }
+  invalidEmail(){
+    this.valid  = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.user.email);
+    return !this.valid;
+  }
   onSubmit(){
     console.log("log in",this.user);
     this.userService.logUser(this.user).subscribe(data=>{
@@ -44,6 +58,20 @@ export class LoginComponent implements OnInit{
       this.error_message = error.error;
       console.log(error);
     })
+  }
+  onSubmitRegister(){
+    if(this.valid){
+    console.log("register",this.user);
+    this.error_message = "";
+    this.userService.addUser(this.user).subscribe(data=>{
+      console.log(data);
+      this.router.navigateByUrl('/login');
+    },
+    error => {
+      console.log(error.error);
+      this.error_message=error.error;
+    })
+  }
   }
 
 }
