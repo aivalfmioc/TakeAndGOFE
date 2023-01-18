1c57fc8c5ea9b955007bb5d036776d652344b494
flavia <flaviamioc@yahoo.com>
2023-01-18 16:12:24 +0200

    styling test1 page

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { User } from '../models/user';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
 })
 export class Test1Component {
   page: number = 1;
+  user : User = new User();
+  constructor(public router: Router, public userService: UserService){
+    this.user=JSON.parse(localStorage.getItem('user')!);
+  }
+  set(){
+    localStorage.setItem('user','hello');
+    this.router.navigateByUrl('/');
+    
+  }
+  ngOnInit(): void{}
+  unset(){
+    localStorage.removeItem('user');
+    window.location.reload();
+  }
   // changeContent(page: number){
   //   this.page = page;
   //   document.getElementById('nav-content')!.style.transform = "translateX(-100%)";
